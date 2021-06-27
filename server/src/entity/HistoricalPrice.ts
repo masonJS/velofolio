dd0a4e3d4043b78a79cb1dd35acc47d96a58bb84
minwoo <mwoodev526@gmail.com>
2021-06-27 19:36:15 +0900

    [create] entity 모델링

---
+++ server/src/entity/HistoricalPrice.ts
@@ -0,0 +1,40 @@
+import {Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn} from 'typeorm'
+import { Stock } from './Stock'
+@Entity({
+  name: 'historical_prices'
+})
+export class HistoricalPrice {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({
+    type: 'varchar',
+    length: 10
+  })
+  type: string
+
+  @Column({ type: 'timestamp' })
+  date: Date
+
+  @Column({ type: 'double' })
+  high: number
+
+  @Column({ type: 'double' })
+  low: number
+
+  @Column({ type: 'double' })
+  open: number
+
+  @Column({ type: 'double' })
+  close: number
+
+  @Column({ type: 'double' })
+  adjusted_close: number
+
+  @Column({ type: 'double' })
+  volume: number
+
+  @ManyToOne(type => Stock, { cascade: true })
+  @JoinColumn({ name: 'stock_id'})
+  stock: Stock
+}
---
+++ server/src/entity/SectorWeighting.ts
@@ -0,0 +1,21 @@
+import {Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn} from 'typeorm'
+import { Stock } from "./Stock";
+
+@Entity({
+  name: 'sector_weightings'
+})
+export class SectorWeighting {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @ManyToOne(type => Stock, { cascade: true })
+  @JoinColumn({ name: 'stock_id' })
+  stock: Stock
+
+  @Column()
+  sector: string
+
+  @Column({ type: 'decimal' })
+  percentage: number
+}
+
